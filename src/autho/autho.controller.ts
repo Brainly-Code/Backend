52cf70dc318324edf8428c34b46dfd65676d815c
IZERE JOSHUA <izerejoshua94@gmail.com>
2025-08-15 21:08:06 +0200

    pull

--- src/autho/autho.controller.ts
+++ src/autho/autho.controller.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
--- src/autho/autho.module.ts
+++ src/autho/autho.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 // src/autho/autho.module.ts
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
